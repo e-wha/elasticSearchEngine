11f71e746fca94f44fb25127f64d0a0a93431eb5
e-wha <ghj4775@naver.com>
2025-04-16 18:37:41 +0900

    init: 초기 환경 설정 [docker]

---
+++ src/test/java/com/elasticsearch/elasticsearchengine/ElasticSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.elasticsearchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
